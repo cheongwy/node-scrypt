479dcabe6699d087634799b1adf8e31807fa67c4
weiyih <cheong@intalio.com>
2011-08-18 16:02:21 +0800

    Fixed inconsistent hash length in base64.

--- scrypt.js
+++ scrypt.js
@@ -1,6 +1,14 @@
 var scrypt = require('./build/default/scrypt');
 
-exports.encrypt = scrypt.encrypt;
+var encrypt = function(password) {
+	var hash = scrypt.encrypt(password);
+	return new Buffer(hash, 'base64').toString('base64');
+};
 
-//var enc = scrypt.encrypt("golden");
-//console.log("Run "+enc);
+//for(var i =0; i< 10; i++) {
+//	var enc = encrypt("golden");
+//	console.log("Hash=> "+enc);
+//	console.log("Length=> "+enc.length);
+//}
+
+exports.encrypt = encrypt; 
